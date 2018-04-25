ff4730cdc1cf57f1ff0af3284a7c13b7d5d98961
ShifatJahan <sjahans.jucse@gmail.com>
2018-04-25 12:06:17 +0600

    hgvfyufyu

--- abc.cpp
+++ abc.cpp
@@ -5,6 +5,12 @@ using namespace std;
 int main()
 {
   cout << "Hello World!";
+<<<<<<< remotes/origin/develop
+=======
+  cout<<"CSE";
+  cout<<"\nshanta";
+  cout<<"\nsetu setu";
+>>>>>>> local
 
   return 0;
 }
